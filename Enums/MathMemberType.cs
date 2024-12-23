976aa90a165330f9a8d0a5aef2d3ba39d82ab9bf
vladimir <vstrukoff@yandex.ru>
2024-12-23 18:38:06 +0300

    Implements Calculator

--- Enums/MathMemberType.cs
+++
@@ -1,9 +0,0 @@
-namespace console_store.Enums;
-
-public enum MathMemberType
-{
-    Operator,
-    Operand,
-    ClosingBracket,
-    OpeningBracket
-}
--- Program.cs
+++ Program.cs
@@ -1,23 +1,9 @@
-﻿using console_store.Servicers;
-
-namespace console_store;
+﻿namespace console_store;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string example = "15^2 + ( ( 1 + 2 ) *  4 ) -3,25";
-        Console.WriteLine($"Enter math expression. Example: {example}");
         
-        string? statement = Console.ReadLine();
-
-        if (statement != null) {
-            try {
-                RpnService service = new RpnService(statement);
-                Console.WriteLine(service.Calculate());
-            } catch {
-                Console.WriteLine("You entered an invalid expression.");
-            }
-        }
     }
 }
